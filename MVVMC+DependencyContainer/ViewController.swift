37b87b8b97b1ea189374af5e95b4daa25003ac31
a0188000 <a0188000@gmail.com>
2021-07-25 19:28:29 +0800

    Initial Commit

---
+++ MVVMC+DependencyContainer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMC+DependencyContainer
+//
+//  Created by 沈維庭 on 2021/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
